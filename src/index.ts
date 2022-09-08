1415fd35ad9de411dd3e502578bde4920161bcd2
Jacob Alford <jacob.alford@me.com>
2022-09-08 17:24:04 -0600

    add index

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+/** @since 0.0.1 */
+export * as S from './SchemableExt'
+
+/** @since 0.0.1 */
+export * as SC from './SchemaExt'
+
+/** @since 0.0.1 */
+export * as D from './Decoder'
+
+/** @since 0.0.1 */
+export * as Eq from './Eq'
+
+/** @since 0.0.1 */
+export * as G from './Guard'
+
+/** @since 0.0.1 */
+export * as TD from './TaskDecoder'
+
+/** @since 0.0.1 */
+export * as t from './Type'
