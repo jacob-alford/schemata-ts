e12797439ff354712a24ec95af680eea60f8f701
newswim <ofenixculpa@gmail.com>
2022-09-26 01:22:46 -0500

    I think i'm getting it

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   collectCoverageFrom: ['src/**'],
-  coverageReporters: ['json-summary'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.ts'],
