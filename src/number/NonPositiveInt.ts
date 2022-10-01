e33b2e8d2329ab42af8fe121a92088ceab65229a
Jacob Alford <jacob.alford@me.com>
2022-10-01 12:53:21 -0600

    [#43] Add NonPositiveIntString string module

--- src/number/NonPositiveInt.ts
+++ src/number/NonPositiveInt.ts
@@ -7,7 +7,7 @@
  *  { z | z ∈ ℤ, z >= -2 ** 53 + 1, z <= 0 }
  * ```
  *
- * @since 0.0.3
+ * @since 0.0.4
  */
 import { Kind, Kind2, URIS, URIS2, HKT2 } from 'fp-ts/HKT'
 import * as D from 'io-ts/Decoder'
@@ -23,7 +23,7 @@ import * as fc from 'fast-check'
 import { isInt } from './Int'
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Internal
  */
 interface NonPositiveIntBrand {
@@ -39,43 +39,43 @@ interface NonPositiveIntBrand {
  *  { z | z ∈ ℤ, z >= -2 ** 53 + 1, z <= 0 }
  * ```
  *
- * @since 0.0.3
+ * @since 0.0.4
  * @category Model
  */
 export type NonPositiveInt = number & NonPositiveIntBrand
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Model
  */
 export type SchemableParams<S> = HKT2<S, number, NonPositiveInt>
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Model
  */
 export type SchemableParams1<S extends URIS> = Kind<S, NonPositiveInt>
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Model
  */
 export type SchemableParams2<S extends URIS2> = Kind2<S, number, NonPositiveInt>
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Model
  */
 export type SchemableParams2C<S extends URIS2> = Kind2<S, unknown, NonPositiveInt>
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Refinements
  */
 export const isNonPositiveInt = (n: number): n is NonPositiveInt => isInt(n) && n <= 0
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const Decoder: SchemableParams2C<D.URI> = pipe(
@@ -84,25 +84,25 @@ export const Decoder: SchemableParams2C<D.URI> = pipe(
 )
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const Encoder: SchemableParams2<Enc.URI> = Enc.id()
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const Eq: SchemableParams1<Eq_.URI> = N.Eq
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const Guard: SchemableParams1<G.URI> = pipe(G.number, G.refine(isNonPositiveInt))
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const TaskDecoder: SchemableParams2C<TD.URI> = pipe(
@@ -111,7 +111,7 @@ export const TaskDecoder: SchemableParams2C<TD.URI> = pipe(
 )
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const Type: SchemableParams1<t.URI> = pipe(
@@ -120,7 +120,7 @@ export const Type: SchemableParams1<t.URI> = pipe(
 )
 
 /**
- * @since 0.0.3
+ * @since 0.0.4
  * @category Instances
  */
 export const Arbitrary: SchemableParams1<Arb.URI> = fc
