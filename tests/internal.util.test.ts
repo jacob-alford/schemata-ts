f3d736d18e5e54e3de96cef99d3f7f28050f33ff
Jacob Alford <jacob.alford@me.com>
2022-10-10 17:36:12 -0600

    [#36] Replace older tests with validator.js test cases (#100)
    * [#36] Refactor ASCII.test.ts
    
    * [#36] Refactor EmailAddress.test.ts
    
    * [#36] Refactor UUID.test.ts, and test-util ZipN

--- tests/internal.util.test.ts
+++ tests/internal.util.test.ts
@@ -1,5 +1,6 @@
 import * as fc from 'fast-check'
 import { base64Encode, digits, urlifyBase64 } from '../src/internal/util'
+import { zipN } from '../test-utils'
 
 describe('base64Encode', () => {
   it('should encode a string', () => {
@@ -28,5 +29,53 @@ describe('digits', () => {
         expect(s).toMatch(/^[0-9]+$/)
       })
     )
+  })
+})
+
+describe('zipN', () => {
+  it("doesn't zip empty", () => {
+    expect(zipN([], [1, 2], [true, false, true])).toStrictEqual([])
+  })
+  it('zips 2', () => {
+    expect(zipN([1, 2, 3], ['a', 'b', 'c', 'd'])).toStrictEqual([
+      [1, 'a'],
+      [2, 'b'],
+      [3, 'c'],
+    ])
+    expect(zipN([1, 2, 3, 4], ['a', 'b', 'c'])).toStrictEqual([
+      [1, 'a'],
+      [2, 'b'],
+      [3, 'c'],
+    ])
+  })
+  it('zips 3', () => {
+    expect(zipN([1, 2, 3], ['a', 'b', 'c', 'd'], [true, false, true])).toStrictEqual([
+      [1, 'a', true],
+      [2, 'b', false],
+      [3, 'c', true],
+    ])
+    expect(zipN([1, 2, 3, 4], ['a', 'b', 'c'], [true, false, true, false])).toStrictEqual(
+      [
+        [1, 'a', true],
+        [2, 'b', false],
+        [3, 'c', true],
+      ]
+    )
+  })
+  it('zips 4', () => {
+    expect(
+      zipN([1, 2, 3], ['a', 'b', 'c', 'd'], [true, false, true], ['x', 'y', 'z', 'w'])
+    ).toStrictEqual([
+      [1, 'a', true, 'x'],
+      [2, 'b', false, 'y'],
+      [3, 'c', true, 'z'],
+    ])
+    expect(
+      zipN([1, 2, 3, 4], ['a', 'b', 'c'], [true, false, true, false], ['x', 'y', 'z'])
+    ).toStrictEqual([
+      [1, 'a', true, 'x'],
+      [2, 'b', false, 'y'],
+      [3, 'c', true, 'z'],
+    ])
   })
 })
