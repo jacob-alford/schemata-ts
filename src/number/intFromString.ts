013ee4ecc5d625d8152db4da7387545b8c7766c1
Jacob Alford <jacob.alford@me.com>
2022-10-26 17:07:42 -0600

    [#124] adjust intFromString to follow encoder/decoder laws

--- src/number/intFromString.ts
+++ src/number/intFromString.ts
@@ -29,46 +29,12 @@ import * as int from './int'
 
 /**
  * Represents all bases numbers can encode to using `Number.prototype.toString(base)`
+ * which have a specified prefix. This is necessary for decoder / encoder laws to hold.
  *
  * @since 1.0.0
  */
 export type IntFromStringParams = {
-  readonly encodeToBase?:
-    | 2
-    | 3
-    | 4
-    | 5
-    | 6
-    | 7
-    | 8
-    | 9
-    | 10
-    | 11
-    | 12
-    | 13
-    | 14
-    | 15
-    | 16
-    | 17
-    | 18
-    | 19
-    | 20
-    | 21
-    | 22
-    | 23
-    | 24
-    | 25
-    | 26
-    | 27
-    | 28
-    | 29
-    | 30
-    | 31
-    | 32
-    | 33
-    | 34
-    | 35
-    | 36
+  readonly encodeToBase?: 2 | 8 | 10 | 16
 }
 
 /**
