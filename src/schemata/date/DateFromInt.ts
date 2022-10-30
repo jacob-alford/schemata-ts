2a6d9f3abb2c523d33efa6aa5065a496f542ca6f
Jacob Alford <jacob.alford@me.com>
2022-10-30 15:10:55 -0600

    [#150] add DateFromInt

---
+++ src/schemata/date/DateFromInt.ts
@@ -0,0 +1,31 @@
+/**
+ * Represents Date objects derived from time in milliseconds.
+ *
+ * @since 1.0.0
+ */
+import { pipe } from 'fp-ts/function'
+import { SchemaExt, make } from '../../SchemaExt'
+import * as int from '../../number/int'
+import * as date from '../../date/date'
+
+/**
+ * Represents Date objects derived from time in milliseconds.
+ *
+ * @since 1.0.0
+ * @category Model
+ */
+export type DateFromInt = SchemaExt<number, Date>
+
+/**
+ * @since 1.0.0
+ * @category Schema
+ */
+export const DateFromInt: SchemaExt<number, Date> = make(S =>
+  pipe(
+    S.int({ min: -8_640_000_000_000_000, max: 8_640_000_000_000_000 }),
+    S.imap(date.Guard, 'DateFromNumber')(
+      n => new Date(n),
+      d => d.getTime() as int.Int
+    )
+  )
+)
