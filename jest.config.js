c823effa7aaf78f2bbaca2040176f699ff285fd7
Jacob Alford <jacob.alford@me.com>
2022-12-06 17:27:12 -0700

    [#213] add coverage to jest and github prs

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 }
