c59d9f4a834de3087eb17d5b83ce4f1edf027b8f
Jacob Alford <jacob.alford@me.com>
2022-12-06 17:16:37 -0700

    [#213] Add Codec

---
+++ src/Codec.ts
@@ -0,0 +1,30 @@
+/**
+ * A Codec is a combined Decoder, Encoder, and Guard.
+ *
+ * @since 1.0.1
+ */
+import { Decoder, getDecoder } from './Decoder'
+import { Encoder, getEncoder } from './Encoder'
+import { getGuard, Guard } from './Guard'
+import { SchemaExt } from './SchemaExt'
+
+/**
+ * A Codec is a combined Decoder, Encoder, and Guard.
+ *
+ * @since 1.0.1
+ * @category Model
+ */
+export interface Codec<E, A>
+  extends Decoder<unknown, A>,
+    Encoder<E, A>,
+    Guard<unknown, A> {}
+
+/**
+ * @since 1.0.1
+ * @category Interpreters
+ */
+export const getCodec = <E, A>(schema: SchemaExt<E, A>): Codec<E, A> => ({
+  ...getDecoder(schema),
+  ...getEncoder(schema),
+  ...getGuard(schema),
+})
