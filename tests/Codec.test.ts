d365e58cf5e8fe859a7effe2c4f592d6fda72c16
Jacob Alford <jacob.alford@me.com>
2022-12-06 17:26:19 -0700

    [#213] undo last two commits

--- tests/Codec.test.ts
+++ tests/Codec.test.ts
@@ -1,7 +1,7 @@
 import { getCodec } from '../src/Codec'
 import * as S from '../src/schemata'
 
-describe.skip('Codec', () => {
+describe('Codec', () => {
   describe('getCodec', () => {
     test('decoding', () => {
       const codec = getCodec(S.FloatFromString())
