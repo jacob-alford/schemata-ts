c94b334df20f63ee99ba86b265ee478dd24dffae
Jacob Alford <jacob.alford@me.com>
2022-12-13 03:32:15 -0700

    experimental schema test

--- src/SchemaExperimental.ts
+++ src/SchemaExperimental.ts
@@ -1,33 +1,67 @@
-import { HKT2 } from 'fp-ts/HKT'
-
+import { Kind, TypeLambda } from '@fp-ts/core/HKT'
 /**
  * @since 2.0.0
  * @category Model
  */
-export interface Schema<R, E, A> {
-  <S>(requirements: R): HKT2<S, E, A>
+export interface Schema<RI extends SchemableLambda, E, A> {
+  <S extends TypeLambda>(requirements: Schemable<RI, S>): Kind<S, never, never, E, A>
 }
+
+/**
+ * @since 2.0.0
+ * @category Schemable
+ */
+export interface SchemableLambda {
+  readonly Kind: TypeLambda
+}
+
+/**
+ * @since 2.0.0
+ * @category Schemable
+ */
+export type Schemable<R extends SchemableLambda, Kind extends TypeLambda> = R extends {
+  readonly type: unknown
+}
+  ? (R & { readonly Kind: Kind })['type']
+  : { readonly R: R; readonly Kind: () => Kind }
+
+type Eq<A, B> = [() => A] extends [() => B]
+  ? [() => B] extends [() => A]
+    ? true
+    : false
+  : false
+
+/**
+ * @since 2.0.0
+ * @category Schemable
+ */
+export type ComposeSchemables<
+  R1 extends SchemableLambda,
+  R2 extends SchemableLambda,
+> = Eq<R1['Kind'], R2['Kind']> extends true ? R1 & R2 : never
 
 /**
  * @since 2.0.0
  * @category Constructors
  */
-export const make: <R, E, A>(s: Schema<R, E, A>) => Schema<R, E, A> = s => s
+export const makeSchema: <R extends SchemableLambda, E, A>(
+  s: Schema<R, E, A>,
+) => Schema<R, E, A> = s => s
 
 /**
  * @since 2.0.0
- * @category Utilites
+ * @category Utilities
  */
 export type RequirementsOf<S> = S extends Schema<infer R, unknown, unknown> ? R : never
 
 /**
  * @since 2.0.0
- * @category Utilites
+ * @category Utilities
  */
-export type InputOf<S> = S extends Schema<unknown, infer E, unknown> ? E : never
+export type InputOf<S> = S extends Schema<any, infer E, unknown> ? E : never
 
 /**
  * @since 2.0.0
- * @category Utilites
+ * @category Utilities
  */
-export type OutputOf<S> = S extends Schema<unknown, unknown, infer A> ? A : never
+export type OutputOf<S> = S extends Schema<any, unknown, infer A> ? A : never
--- src/schemables/WithPrimitives/definition.ts
+++ src/schemables/WithPrimitives/definition.ts
@@ -1,168 +1,86 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HKT2, Kind, Kind2, URIS, URIS2 } from 'fp-ts/HKT'
+import { Kind, TypeClass, TypeLambda } from '@fp-ts/core/HKT'
 
-import { InputOf, make, OutputOf, RequirementsOf, Schema } from '../../SchemaExperimental'
+import {
+  ComposeSchemables,
+  InputOf,
+  makeSchema,
+  OutputOf,
+  RequirementsOf,
+  Schema,
+  SchemableLambda,
+} from '../../SchemaExperimental'
 
-/** @internal */
-const StringURI = Symbol('WithPrimitives.String')
+const PrimitiveURI = Symbol('Primitives')
+type PrimitiveURI = typeof PrimitiveURI
+
+const StringURI = Symbol('Primitives.String')
 type StringURI = typeof StringURI
 
-/** @internal */
-const NumberURI = Symbol('WithPrimitives.Number')
+const NumberURI = Symbol('Primitives.Number')
 type NumberURI = typeof NumberURI
 
-/** @internal */
-const BooleanURI = Symbol('WithPrimitives.Boolean')
+const BooleanURI = Symbol('Primitives.Boolean')
 type BooleanURI = typeof BooleanURI
 
-/** @internal */
-const StructURI = Symbol('WithPrimitives.Struct')
+const StructURI = Symbol('Primitives.Struct')
 type StructURI = typeof StructURI
 
-/** @internal */
-const ArrayURI = Symbol('WithPrimitives.Array')
+const ArrayURI = Symbol('Primitives.Array')
 type ArrayURI = typeof ArrayURI
 
-type InnerLeftG<S, E extends HKT2<S, any, any>> = E extends HKT2<S, infer L, any>
-  ? L
-  : never
-type InnerRightG<S, A extends HKT2<S, any, any>> = A extends HKT2<S, any, infer R>
-  ? R
-  : never
-
-type InnerLeft<S extends URIS2, E extends Kind2<S, any, any>> = E extends Kind2<
-  S,
-  infer L,
-  any
->
-  ? L
-  : never
-type InnerRight<S extends URIS2, A extends Kind2<S, any, any>> = A extends Kind2<
-  S,
-  any,
-  infer R
->
-  ? R
-  : never
+type InnerLeft<
+  S extends TypeLambda,
+  E extends Kind<S, any, any, any, any>,
+> = E extends Kind<S, any, any, infer L, any> ? L : never
+type InnerRight<
+  S extends TypeLambda,
+  A extends Kind<S, any, any, any, any>,
+> = A extends Kind<S, any, any, any, infer R> ? R : never
 
 /**
  * @since 2.0.0
  * @category Model
  */
-export interface WithPrimitives<S> {
-  readonly [StringURI]: HKT2<S, string, string>
-  readonly [NumberURI]: HKT2<S, number, number>
-  readonly [BooleanURI]: HKT2<S, boolean, boolean>
-  readonly [StructURI]: <P extends Record<string, HKT2<S, any, any>>>(
+export interface WithPrimitives<S extends TypeLambda> extends TypeClass<S> {
+  readonly [StringURI]: Kind<S, never, never, string, string>
+  readonly [NumberURI]: Kind<S, never, never, number, number>
+  readonly [BooleanURI]: Kind<S, never, never, boolean, boolean>
+  readonly [StructURI]: <P extends Record<string, Kind<S, any, any, any, any>>>(
     properties: P,
-  ) => HKT2<
+  ) => Kind<
     S,
-    { [K in keyof P]: InnerLeftG<S, P[K]> },
-    { [K in keyof P]: InnerRightG<S, P[K]> }
-  >
-  readonly [ArrayURI]: <O, A>(
-    item: HKT2<S, O, A>,
-  ) => HKT2<S, ReadonlyArray<O>, ReadonlyArray<A>>
-}
-
-/**
- * @since 2.0.0
- * @category Model
- */
-export interface WithPrimitives1<S extends URIS> {
-  readonly [StringURI]: Kind<S, string>
-  readonly [NumberURI]: Kind<S, number>
-  readonly [BooleanURI]: Kind<S, boolean>
-  readonly [StructURI]: <P extends Record<string, Kind<S, any>>>(
-    properties: P,
-  ) => Kind<S, { [K in keyof P]: P[K] }>
-  readonly [ArrayURI]: <A>(item: Kind<S, A>) => Kind<S, ReadonlyArray<A>>
-}
-
-/**
- * @since 2.0.0
- * @category Model
- */
-export interface WithPrimitives2<S extends URIS2> {
-  readonly [StringURI]: Kind2<S, string, string>
-  readonly [NumberURI]: Kind2<S, number, number>
-  readonly [BooleanURI]: Kind2<S, boolean, boolean>
-  readonly [StructURI]: <P extends Record<string, Kind2<S, any, any>>>(
-    properties: P,
-  ) => Kind2<
-    S,
+    never,
+    never,
     { [K in keyof P]: InnerLeft<S, P[K]> },
     { [K in keyof P]: InnerRight<S, P[K]> }
   >
   readonly [ArrayURI]: <O, A>(
-    item: Kind2<S, O, A>,
-  ) => Kind2<S, ReadonlyArray<O>, ReadonlyArray<A>>
+    item: Kind<S, never, never, O, A>,
+  ) => Kind<S, never, never, ReadonlyArray<O>, ReadonlyArray<A>>
 }
 
 /**
  * @since 2.0.0
- * @category Model
+ * @category Type Lambdas
  */
-export interface WithPrimitives2C<S extends URIS2, E> {
-  readonly [StringURI]: Kind2<S, E, string>
-  readonly [NumberURI]: Kind2<S, E, number>
-  readonly [BooleanURI]: Kind2<S, E, boolean>
-  readonly [StructURI]: <A>(properties: { [K in keyof A]: Kind2<S, E, A[K]> }) => Kind2<
-    S,
-    E,
-    { [K in keyof A]: A[K] }
-  >
-  readonly [ArrayURI]: <A>(item: Kind2<S, E, A>) => Kind2<S, E, ReadonlyArray<A>>
+export interface WithPrimitivesLambda extends SchemableLambda {
+  readonly type: WithPrimitives<this['Kind']>
+  readonly key: PrimitiveURI
 }
 
 /**
  * @since 2.0.0
  * @category Constructors
  */
-export const make1 = <S extends URIS>(
-  string: WithPrimitives1<S>[StringURI],
-  number: WithPrimitives1<S>[NumberURI],
-  boolean: WithPrimitives1<S>[BooleanURI],
-  struct: WithPrimitives1<S>[StructURI],
-  array: WithPrimitives1<S>[ArrayURI],
-): WithPrimitives1<S> => ({
-  [StringURI]: string,
-  [NumberURI]: number,
-  [BooleanURI]: boolean,
-  [StructURI]: struct,
-  [ArrayURI]: array,
-})
-
-/**
- * @since 2.0.0
- * @category Constructors
- */
-export const make2 = <S extends URIS2>(
-  string: WithPrimitives2<S>[StringURI],
-  number: WithPrimitives2<S>[NumberURI],
-  boolean: WithPrimitives2<S>[BooleanURI],
-  struct: WithPrimitives2<S>[StructURI],
-  array: WithPrimitives2<S>[ArrayURI],
-): WithPrimitives2<S> => ({
-  [StringURI]: string,
-  [NumberURI]: number,
-  [BooleanURI]: boolean,
-  [StructURI]: struct,
-  [ArrayURI]: array,
-})
-
-/**
- * @since 2.0.0
- * @category Constructors
- */
-export const make2C = <S extends URIS2, E>(
-  string: WithPrimitives2C<S, E>[StringURI],
-  number: WithPrimitives2C<S, E>[NumberURI],
-  boolean: WithPrimitives2C<S, E>[BooleanURI],
-  struct: WithPrimitives2C<S, E>[StructURI],
-  array: WithPrimitives2C<S, E>[ArrayURI],
-): WithPrimitives2C<S, E> => ({
+export const make = <S extends TypeLambda>(
+  string: WithPrimitives<S>[StringURI],
+  number: WithPrimitives<S>[NumberURI],
+  boolean: WithPrimitives<S>[BooleanURI],
+  struct: WithPrimitives<S>[StructURI],
+  array: WithPrimitives<S>[ArrayURI],
+): WithPrimitives<S> => ({
   [StringURI]: string,
   [NumberURI]: number,
   [BooleanURI]: boolean,
@@ -174,25 +92,21 @@ export const make2C = <S extends URIS2, E>(
  * @since 2.0.0
  * @category Schemata
  */
-export const String = <R extends WithPrimitives<unknown>>(): Schema<R, string, string> =>
-  make(S => S[StringURI])
+export const String = makeSchema<WithPrimitivesLambda, string, string>(S => S[StringURI])
 
 /**
  * @since 2.0.0
  * @category Schemata
  */
-export const Number = <R extends WithPrimitives<unknown>>(): Schema<R, number, number> =>
-  make(S => S[NumberURI])
+export const Number = makeSchema<WithPrimitivesLambda, number, number>(S => S[NumberURI])
 
 /**
  * @since 2.0.0
  * @category Schemata
  */
-export const Boolean = <R extends WithPrimitives<unknown>>(): Schema<
-  R,
-  boolean,
-  boolean
-> => make(S => S[BooleanURI])
+export const Boolean = makeSchema<WithPrimitivesLambda, boolean, boolean>(
+  S => S[BooleanURI],
+)
 
 /**
  * @since 2.0.0
@@ -205,7 +119,11 @@ export const Struct = <P extends Record<string, Schema<any, any, any>>>(
   { [K in keyof P]: InputOf<P[K]> },
   { [K in keyof P]: OutputOf<P[K]> }
 > =>
-  make(s => {
+  makeSchema<
+    { [K in keyof P]: RequirementsOf<P[K]> }[keyof P],
+    { [K in keyof P]: InputOf<P[K]> },
+    { [K in keyof P]: OutputOf<P[K]> }
+  >(s => {
     const out = {} as any
     for (const k in properties) {
       out[k] = properties[k]!(s)
@@ -217,6 +135,15 @@ export const Struct = <P extends Record<string, Schema<any, any, any>>>(
  * @since 2.0.0
  * @category Schemata
  */
-export const Array = <R extends WithPrimitives<unknown>, E, A>(
+export const Array = <R extends SchemableLambda, E, A>(
   item: Schema<R, E, A>,
-): Schema<R, ReadonlyArray<E>, ReadonlyArray<A>> => make(_ => _[ArrayURI](item(_)))
+): Schema<
+  ComposeSchemables<WithPrimitivesLambda, R>,
+  ReadonlyArray<E>,
+  ReadonlyArray<A>
+> =>
+  makeSchema<
+    ComposeSchemables<WithPrimitivesLambda, R>,
+    ReadonlyArray<E>,
+    ReadonlyArray<A>
+  >(S => S[ArrayURI](item(S)))
