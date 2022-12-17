2023bc286230352d0f33ef78fb2965dd013acb4d
Dan Minshew <ofenixculpa@gmail.com>
2022-12-17 11:19:25 -0600

    [#163] Add `NonEmptyArray` generic schema (#223)
    * (package.json) remove unused scripts
    
    * [#163] Add `NonEmptyArray` generic schema
    
    * [#163] Add test cases

---
+++ src/schemata/generic/NonEmptyArray.ts
@@ -0,0 +1,27 @@
+/**
+ * A read-only Array containing one or more elements.
+ *
+ * @since 1.0.2
+ */
+import { pipe } from 'fp-ts/function'
+import * as RA from 'fp-ts/ReadonlyArray'
+import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
+
+import { make, SchemaExt } from '../../SchemaExt'
+
+/**
+ * @since 1.0.2
+ * @category Model
+ */
+export type NonEmptyArrayS = <A, O>(
+  sA: SchemaExt<O, A>,
+) => SchemaExt<Array<O>, RNEA.ReadonlyNonEmptyArray<A>>
+
+/**
+ * A read-only Array containing one or more elements.
+ *
+ * @since 1.0.2
+ * @category Schema
+ */
+export const NonEmptyArray: NonEmptyArrayS = sA =>
+  make(S => pipe(S.array(sA(S)), S.readonly, S.refine(RA.isNonEmpty, 'NonEmptyArray')))
