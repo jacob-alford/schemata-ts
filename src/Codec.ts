b37e3d5834a55d4e607a1eeec942788f8cefb40a
Jacob Alford <jacob.alford@me.com>
2022-12-18 17:32:46 -0700

    [#211] Add JsonSerializer/Deserializer, add to codec

--- src/Codec.ts
+++ src/Codec.ts
@@ -1,25 +1,29 @@
 /**
- * A Codec is a combined Decoder, Encoder, and Guard.
+ * A Codec is a combined Decoder, Encoder, Guard, JsonSerializer, and JsonDeserializer.
  *
  * @since 1.0.1
  */
 import { Decoder, getDecoder } from './Decoder'
 import { Encoder, getEncoder } from './Encoder'
 import { getGuard, Guard } from './Guard'
+import { getJsonDeserializer, JsonDeserializer } from './JsonDeserializer'
+import { getJsonSerializer, JsonSerializer } from './JsonSerializer'
 import { SchemaExt } from './SchemaExt'
 
 /**
- * A Codec is a combined Decoder, Encoder, and Guard.
- *
  * @since 1.0.1
  * @category Model
  */
 export interface Codec<E, A>
   extends Decoder<unknown, A>,
     Encoder<E, A>,
-    Guard<unknown, A> {}
+    Guard<unknown, A>,
+    JsonSerializer<A>,
+    JsonDeserializer<A> {}
 
 /**
+ * A Codec is a combined Decoder, Encoder, Guard, JsonSerializer, and JsonDeserializer.
+ *
  * @since 1.0.1
  * @category Interpreters
  */
@@ -27,4 +31,6 @@ export const getCodec = <E, A>(schema: SchemaExt<E, A>): Codec<E, A> => ({
   ...getDecoder(schema),
   ...getEncoder(schema),
   ...getGuard(schema),
+  ...getJsonSerializer(schema),
+  ...getJsonDeserializer(schema),
 })
