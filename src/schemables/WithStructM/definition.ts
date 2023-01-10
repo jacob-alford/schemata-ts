5e1859f54e5cd475e36c1c2115d2eb5e40b1ef5e
Jacob Alford <jacob.alford@me.com>
2023-01-09 19:29:08 -0700

    [#136] finish mapped-struct poc

--- src/schemables/WithStructM/definition.ts
+++ src/schemables/WithStructM/definition.ts
@@ -1,124 +1,268 @@
 /**
- * Schemable construction based on Regex combinators
+ * Schemable for mapping a struct
  *
  * @since 1.3.0
  */
 import { HKT2, Kind, Kind2, URIS, URIS2 } from 'fp-ts/HKT'
-import * as Enc from 'schemata-ts/base/EncoderBase'
+
+const OptionalKeyFlag = Symbol()
+type OptionalKeyFlag = typeof OptionalKeyFlag
+
+const RequiredKeyFlag = Symbol()
+type RequiredKeyFlag = typeof RequiredKeyFlag
+
+const KeyNotMapped = Symbol()
+type KeyNotMapped = typeof KeyNotMapped
 
 /**
  * @since 1.3.0
  * @category Model
  */
-export type KeyFlag = 'required' | 'optional'
+export type KeyFlag = OptionalKeyFlag | RequiredKeyFlag
 
-type Prop<S> = readonly [val: HKT2<S, any, any>]
-type PropWithKeyFlag<S> = readonly [flag: KeyFlag, val: HKT2<S, any, any>]
-type PropWithMappedKeyFlag<S, K extends string> = readonly [
-  flag: KeyFlag,
-  keyOut: K,
-  val: HKT2<S, any, any>,
-]
+/**
+ * @since 1.3.0
+ * @category Model
+ */
+export type Prop<
+  Flag extends KeyFlag,
+  S,
+  Val extends HKT2<S, any, any>,
+  K extends string | KeyNotMapped,
+> = readonly [Flag, K, Val]
+
+/**
+ * @since 1.3.0
+ * @category Model
+ */
+export type Prop1<
+  Flag extends KeyFlag,
+  S extends URIS,
+  Val extends Kind<S, any>,
+  K extends string | KeyNotMapped,
+> = readonly [Flag, K, Val]
+
+/**
+ * @since 1.3.0
+ * @category Model
+ */
+export type Prop2<
+  Flag extends KeyFlag,
+  S extends URIS2,
+  Val extends Kind2<S, any, any>,
+  K extends string | KeyNotMapped,
+> = readonly [Flag, K, Val]
+
+type Required = {
+  <S extends URIS2, Val extends Kind2<S, any, any>>(val: Val): Prop2<
+    RequiredKeyFlag,
+    S,
+    Val,
+    KeyNotMapped
+  >
+  <S extends URIS, Val extends Kind<S, any>>(val: Val): Prop1<
+    RequiredKeyFlag,
+    S,
+    Val,
+    KeyNotMapped
+  >
+  <S, Val extends HKT2<S, any, any>>(val: Val): Prop<
+    RequiredKeyFlag,
+    S,
+    Val,
+    KeyNotMapped
+  >
+}
+
+/**
+ * @since 1.3.0
+ * @category Constructors
+ */
+export const required: Required = (val: any) =>
+  [RequiredKeyFlag, KeyNotMapped, val] as any
+
+/**
+ * @since 1.3.0
+ * @category Constructors
+ */
+export const isRequiredFlag = (flag: KeyFlag): flag is RequiredKeyFlag =>
+  flag === RequiredKeyFlag
+
+type Optional = {
+  <S extends URIS2, Val extends Kind2<S, any, any>>(val: Val): Prop2<
+    OptionalKeyFlag,
+    S,
+    Val,
+    KeyNotMapped
+  >
+  <S extends URIS, Val extends Kind<S, any>>(val: Val): Prop1<
+    OptionalKeyFlag,
+    S,
+    Val,
+    KeyNotMapped
+  >
+  <S, Val extends HKT2<S, any, any>>(val: Val): Prop<
+    OptionalKeyFlag,
+    S,
+    Val,
+    KeyNotMapped
+  >
+}
+
+/**
+ * @since 1.3.0
+ * @category Constructors
+ */
+export const optional: Optional = (val: any) =>
+  [OptionalKeyFlag, KeyNotMapped, val] as any
+
+type MapKeyTo = {
+  <K extends string>(mapTo: K): <
+    Flag extends KeyFlag,
+    S extends URIS2,
+    Val extends Kind2<S, any, any>,
+  >(
+    prop: Prop2<Flag, S, Val, KeyNotMapped>,
+  ) => Prop<Flag, S, Val, K>
+  <K extends string>(mapTo: K): <
+    Flag extends KeyFlag,
+    S extends URIS,
+    Val extends Kind<S, any>,
+  >(
+    prop: Prop1<Flag, S, Val, KeyNotMapped>,
+  ) => Prop1<Flag, S, Val, K>
+  <K extends string>(mapTo: K): <Flag extends KeyFlag, S, Val extends HKT2<S, any, any>>(
+    prop: Prop<Flag, S, Val, KeyNotMapped>,
+  ) => Prop<Flag, S, Val, K>
+}
+
+/**
+ * @since 1.3.0
+ * @category Combinators
+ */
+export const mapKeyTo: MapKeyTo = mapTo => (prop: any) => [prop[0], mapTo, prop[2]] as any
+
+/**
+ * @since 1.3.0
+ * @category Constructors
+ */
+export const isOptionalFlag = (flag: KeyFlag): flag is OptionalKeyFlag =>
+  flag === OptionalKeyFlag
+
+type Combine<A> = {
+  [K in keyof A]: A[K]
+} extends infer B
+  ? B
+  : never
 
 /**
  * @since 1.3.0
  * @category Model
  */
 export interface WithStructM<S> {
-  readonly structM: <
-    Props extends Record<
-      string,
-      | (() => Prop<S>)
-      | (() => PropWithKeyFlag<S>)
-      | (<K extends string>() => PropWithMappedKeyFlag<S, K>)
-    >,
-  >(
+  readonly structM: <Props extends Record<string, Prop<any, S, HKT2<S, any, any>, any>>>(
     properties: Props,
   ) => HKT2<
     S,
-    {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
-        ? Flag extends 'required'
-          ? K
+    Combine<
+      {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
+          ? Flag extends RequiredKeyFlag
+            ? K
+            : never
+          : never]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends HKT2<S, infer E, any>
+            ? E
+            : never
           : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'required'
-          ? K
+      } & {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
+          ? Flag extends OptionalKeyFlag
+            ? K
+            : never
+          : never]?: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends HKT2<S, infer E, any>
+            ? E
+            : never
           : never
-        : never]: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends HKT2<S, infer E, any>
-          ? E
+      }
+    >,
+    Combine<
+      {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends RequiredKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends HKT2<S, any, infer A>
+            ? A
+            : never
           : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends HKT2<S, infer E, any>
-          ? E
+      } & {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends OptionalKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : K]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends HKT2<S, any, infer A>
+            ? A
+            : never
           : never
-        : never
-    } & {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
-        ? Flag extends 'optional'
-          ? K
-          : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'optional'
-          ? K
-          : never
-        : never]: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends HKT2<S, infer E, any>
-          ? E
-          : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends HKT2<S, infer E, any>
-          ? E
-          : never
-        : never
-    },
-    {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
-        ? Flag extends 'required'
-          ? KOut
-          : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'required'
-          ? K
-          : never
-        : K]: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends HKT2<S, any, infer A>
-          ? A
-          : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends HKT2<S, any, infer A>
-          ? A
-          : never
-        : never
-    } & {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
-        ? Flag extends 'optional'
-          ? KOut
-          : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'optional'
-          ? K
-          : never
-        : K]: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends HKT2<S, any, infer A>
-          ? A
-          : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends HKT2<S, any, infer A>
-          ? A
-          : never
-        : never
-    }
+      }
+    >
   >
 }
 
-type PropWithKeyFlag2<S extends URIS2> = readonly [flag: KeyFlag, val: Kind2<S, any, any>]
-type PropWithMappedKeyFlag2<S extends URIS2, K extends string> = readonly [
-  flag: KeyFlag,
-  keyOut: K,
-  val: Kind2<S, any, any>,
-]
+/**
+ * @since 1.3.0
+ * @category Model
+ */
+export interface WithStructM1<S extends URIS> {
+  readonly structM: <Props extends Record<string, Prop1<any, S, Kind<S, any>, any>>>(
+    properties: Props,
+  ) => Kind<
+    S,
+    Combine<
+      {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends RequiredKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind<S, infer A>
+            ? A
+            : never
+          : never
+      } & {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends OptionalKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]?: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind<S, infer A>
+            ? A
+            : never
+          : never
+      }
+    >
+  >
+}
 
 /**
  * @since 1.3.0
@@ -126,100 +270,110 @@ type PropWithMappedKeyFlag2<S extends URIS2, K extends string> = readonly [
  */
 export interface WithStructM2<S extends URIS2> {
   readonly structM: <
-    Props extends Record<string, PropWithKeyFlag2<S> | PropWithMappedKeyFlag2<S, string>>,
+    Props extends Record<string, Prop2<any, S, Kind2<S, any, any>, any>>,
   >(
     properties: Props,
   ) => Kind2<
     S,
-    {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
-        ? Flag extends 'required'
-          ? K
+    Combine<
+      {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
+          ? Flag extends RequiredKeyFlag
+            ? K
+            : never
+          : never]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind2<S, infer E, any>
+            ? E
+            : never
           : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'required'
-          ? K
+      } & {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
+          ? Flag extends OptionalKeyFlag
+            ? K
+            : never
+          : never]?: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind2<S, infer E, any>
+            ? E
+            : never
           : never
-        : never]: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends Kind2<S, infer E, any>
-          ? E
+      }
+    >,
+    Combine<
+      {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends RequiredKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind2<S, any, infer A>
+            ? A
+            : never
           : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends Kind2<S, infer E, any>
-          ? E
+      } & {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends OptionalKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]?: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind2<S, any, infer A>
+            ? A
+            : never
           : never
-        : never
-    } & {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, any, any]
-        ? Flag extends 'optional'
-          ? K
-          : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'optional'
-          ? K
-          : never
-        : never]+?: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends Kind2<S, infer E, any>
-          ? E
-          : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends Kind2<S, infer E, any>
-          ? E
-          : never
-        : never
-    },
-    {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
-        ? Flag extends 'required'
-          ? KOut
-          : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'required'
-          ? K
-          : never
-        : never]: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends Kind2<S, any, infer A>
-          ? A
-          : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends Kind2<S, any, infer A>
-          ? A
-          : never
-        : never
-    } & {
-      [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
-        ? Flag extends 'optional'
-          ? KOut
-          : never
-        : Props[K] extends readonly [infer Flag, any]
-        ? Flag extends 'optional'
-          ? K
-          : never
-        : never]+?: Props[K] extends readonly [any, any, infer Val]
-        ? Val extends Kind2<S, any, infer A>
-          ? A
-          : never
-        : Props[K] extends readonly [any, infer Val]
-        ? Val extends Kind2<S, any, infer A>
-          ? A
-          : never
-        : never
-    }
+      }
+    >
   >
 }
 
-declare const encodeStructM: WithStructM2<Enc.URI>['structM']
-
-const keyWithFlag: <A, B>(a: A, b: B) => readonly [A, B] = (a, b) => [a, b]
-const keyWithMappedFlag: <A extends KeyFlag, B extends string, C>(
-  a: A,
-  b: B,
-  c: C,
-) => readonly [A, B, C] = (a, b, c) => [a, b, c]
-
-const test = encodeStructM({
-  a: keyWithFlag('optional', Enc.Schemable.string),
-  b: keyWithMappedFlag('required', 'c', Enc.Schemable.number),
-})
-
-const out = test.encode({ c: 5 })
+/**
+ * @since 1.3.0
+ * @category Model
+ */
+export interface WithStructM2C<S extends URIS2, E> {
+  readonly structM: <
+    Props extends Record<string, Prop2<any, S, Kind2<S, any, any>, any>>,
+  >(
+    properties: Props,
+  ) => Kind2<
+    S,
+    E,
+    Combine<
+      {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends RequiredKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind2<S, any, infer A>
+            ? A
+            : never
+          : never
+      } & {
+        [K in keyof Props as Props[K] extends readonly [infer Flag, infer KOut, any]
+          ? Flag extends OptionalKeyFlag
+            ? KOut extends KeyNotMapped
+              ? K
+              : KOut extends keyof Props
+              ? K
+              : KOut
+            : never
+          : never]?: Props[K] extends readonly [any, any, infer Val]
+          ? Val extends Kind2<S, any, infer A>
+            ? A
+            : never
+          : never
+      }
+    >
+  >
+}
