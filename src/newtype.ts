5915beb421110d6c2837f6b8526d769ec9ab7042
Jacob Alford <jacob.alford@me.com>
2023-02-12 12:34:13 -0700

    [#256] add newtype module

---
+++ src/newtype.ts
@@ -0,0 +1,61 @@
+/**
+ * A port of `newtype-ts` to schemata-ts
+ *
+ * @since 1.4.0
+ */
+
+import { unsafeCoerce } from 'fp-ts/function'
+import { Iso, make } from 'schemata-ts/iso'
+
+/**
+ * Represents a wrapped type that's not assignable to its underlying type.
+ *
+ * @since 1.4.0
+ * @category Models
+ */
+export interface Newtype<URI, A> {
+  readonly _URI: URI
+  readonly _A: A
+}
+
+/**
+ * @since 1.4.0
+ * @category Type Helpers
+ */
+export type URIOf<N extends Newtype<any, any>> = N['_URI']
+
+/**
+ * @since 1.4.0
+ * @category Type Helpers
+ */
+export type CarrierOf<N extends Newtype<any, any>> = N['_A']
+
+/**
+ * @since 1.4.0
+ * @category Type Helpers
+ */
+export type CombineURIs<
+  N1 extends Newtype<any, any>,
+  N2 extends Newtype<any, CarrierOf<N1>>,
+> = Newtype<URIOf<N1> & URIOf<N2>, CarrierOf<N1>>
+
+/**
+ * @since 1.4.0
+ * @category Constructors
+ */
+export const iso: <Nt extends Newtype<any, any>>() => Iso<Nt, CarrierOf<Nt>> = () =>
+  make(unsafeCoerce, unsafeCoerce)
+
+/**
+ * @since 1.4.0
+ * @category Constructors
+ */
+export const wrap: <Nt extends Newtype<any, any>>() => (a: CarrierOf<Nt>) => Nt = () =>
+  unsafeCoerce
+
+/**
+ * @since 1.4.0
+ * @category Destructors
+ */
+export const unwrap: <Nt extends Newtype<any, any>>() => (a: Nt) => CarrierOf<Nt> = () =>
+  unsafeCoerce
