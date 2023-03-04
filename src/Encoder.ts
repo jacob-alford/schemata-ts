392d91d00adb18a4b337cf0936ab51fbc74c89d2
Jacob Alford <jacob.alford@me.com>
2023-03-04 10:09:55 -0700

    wip -- add encoder and eq

--- src/Encoder.ts
+++ src/Encoder.ts
@@ -4,6 +4,12 @@
  *
  * @since 2.0.0
  */
+import { Category2 } from 'fp-ts/Category'
+import { Contravariant2 } from 'fp-ts/Contravariant'
+import { flow, identity } from 'fp-ts/function'
+import { Invariant2 } from 'fp-ts/Invariant'
+import { Semigroupoid2 } from 'fp-ts/Semigroupoid'
+import * as hkt from 'schemata-ts/HKT'
 
 /**
  * Represents a typeclass and data type for encoding values that are the transformed
@@ -13,33 +19,110 @@
  * @category Model
  */
 export interface Encoder<I, O> {
-  readonly encode: (a: I) => O
+  readonly encode: (a: O) => I
+}
+
+// ------------------
+// combinators
+// ------------------
+
+export {
+  /**
+   * Interprets a schema as a decoder
+   *
+   * @since 2.0.0
+   * @category Interpreters
+   */
+  getEncoder,
+} from 'schemata-ts/derivations/EncoderSchemable'
+
+// ------------------
+// instances
+// ------------------
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const URI = 'schemata-ts/Encoder'
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export type URI = typeof URI
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind2<E, A> {
+    readonly [URI]: Encoder<E, A>
+  }
 }
 
 /**
- * An instance of `Schemable` for io-ts/Encoder
- *
- * @since 1.0.0
+ * @since 2.0.0
+ * @category Type Lambdas
+ */
+export interface TypeLambda extends hkt.TypeLambda {
+  readonly type: Encoder<this['In'], this['Target']>
+}
+
+/**
+ * @since 2.0.0
+ * @category Type Lambdas
+ */
+export interface SchemableLambda extends hkt.SchemableLambda {
+  readonly type: Encoder<this['Input'], this['Output']>
+}
+
+// non-pipeables
+const contramap_: Contravariant2<URI>['contramap'] = (fa, f) => ({
+  encode: flow(f, fa.encode),
+})
+const compose_: Semigroupoid2<URI>['compose'] = (bc, ab) => ({
+  encode: flow(bc.encode, ab.encode),
+})
+const imap_: Invariant2<URI>['imap'] = (fa, _, g) => contramap_(fa, g)
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const imap: <A, B>(
+  f: (a: A) => B,
+  g: (b: B) => A,
+) => <I>(fa: Encoder<I, A>) => Encoder<I, B> = (f, g) => fa => imap_(fa, f, g)
+
+/**
+ * @since 2.0.0
  * @category Instances
  */
-export const Schemable: Schemable2<Enc.URI> = {
-  URI: Enc.URI,
-  literal: () => Enc.id(),
-  string: Enc.id(),
-  number: Enc.id(),
-  boolean: Enc.id(),
-  nullable: Enc.nullable,
-  // @ts-expect-error -- typelevel difference
-  struct: Enc.struct,
-  // @ts-expect-error -- typelevel difference
-  partial: Enc.partial,
-  record: Enc.record,
-  array: Enc.array,
-  // @ts-expect-error -- typelevel difference
-  tuple: Enc.tuple,
-  intersect: Enc.intersect,
-  // @ts-expect-error -- typelevel difference
-  sum: Enc.sum,
-  lazy: (_id, f) => Enc.lazy(f),
-  readonly: Enc.readonly,
+export const Invariant: Invariant2<URI> = {
+  URI,
+  imap: imap_,
+}
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const compose: <B, C>(
+  ab: Encoder<B, C>,
+) => <A>(bc: Encoder<A, B>) => Encoder<A, C> = bc => ab => compose_(bc, ab)
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const id: Category2<URI>['id'] = () => ({
+  encode: identity,
+})
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const Category: Category2<URI> = {
+  URI,
+  compose: compose_,
+  id,
 }
--- src/Eq.ts
+++ src/Eq.ts
@@ -1,13 +1,182 @@
 /**
- * Re-exports for io-ts/Eq
+ * Represents a typeclass and data type that determines if two values of the same type are
+ * equal, and follows the following laws:
  *
- * @since 1.0.0
+ * 1. Reflexivity: `x === x`
+ * 2. Symmetry: `x === y <=> y === x`
+ * 3. Transitivity: `x === y && y === z => x === z`
+ *
+ * @since 2.0.0
  */
-export type {
+import { Contravariant1 } from 'fp-ts/Contravariant'
+import { Invariant1 } from 'fp-ts/Invariant'
+import { Monoid } from 'fp-ts/Monoid'
+import * as hkt from 'schemata-ts/HKT'
+
+/**
+ * Represents a typeclass and data type that determines if two values of the same type are
+ * equal, and follows the following laws:
+ *
+ * 1. Reflexivity: `x === x`
+ * 2. Symmetry: `x === y <=> y === x`
+ * 3. Transitivity: `x === y && y === z => x === z`
+ *
+ * @since 2.0.0
+ */
+export interface Eq<A> {
+  readonly equals: (x: A, y: A) => boolean
+}
+
+// ------------------
+// constructors
+// ------------------
+
+/**
+ * @since 2.0.0
+ * @category Constructors
+ */
+export const fromEquals = (equals: Eq<any>['equals']): Eq<any> => ({
+  equals: (x, y) => x === y || equals(x, y),
+})
+
+// ------------------
+// combinators
+// ------------------
+
+export {
   /**
-   * @since 1.0.0
-   * @category Model
+   * Interprets a schema as a decoder
+   *
+   * @since 2.0.0
+   * @category Interpreters
    */
-  Eq,
-} from 'fp-ts/Eq'
-export * from 'io-ts/Eq'
+  getEq,
+} from 'schemata-ts/derivations/EqSchemable'
+
+// ------------------
+// instances
+// ------------------
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const URI = 'schemata-ts/Encoder'
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export type URI = typeof URI
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind<A> {
+    readonly [URI]: Eq<A>
+  }
+}
+
+/**
+ * @since 2.0.0
+ * @category Type Lambdas
+ */
+export interface TypeLambda extends hkt.TypeLambda {
+  readonly type: Eq<this['Target']>
+}
+
+/**
+ * @since 2.0.0
+ * @category Type Lambdas
+ */
+export interface SchemableLambda extends hkt.SchemableLambda {
+  readonly type: Eq<this['Output']>
+}
+
+// non-pipeables
+const contramap_: Contravariant1<URI>['contramap'] = (fa, f) =>
+  fromEquals((x, y) => fa.equals(f(x), f(y)))
+const imap_: Invariant1<URI>['imap'] = (fa, _, g) => contramap_(fa, g)
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const imap: <A, B>(f: (a: A) => B, g: (b: B) => A) => (fa: Eq<A>) => Eq<B> =
+  (f, g) => fa =>
+    imap_(fa, f, g)
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const Invariant: Invariant1<URI> = {
+  URI,
+  imap: imap_,
+}
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const contramap: <A, B>(f: (b: B) => A) => (fa: Eq<A>) => Eq<B> = f => fa =>
+  contramap_(fa, f)
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const Contravariant: Contravariant1<URI> = {
+  URI,
+  contramap: contramap_,
+}
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const and: <A>(that: Eq<A>) => (self: Eq<A>) => Eq<A> = that => self =>
+  fromEquals((x, y) => self.equals(x, y) && that.equals(x, y))
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const always: Eq<unknown> = fromEquals(() => true)
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const getMonoidAll = <A>(): Monoid<Eq<A>> => ({
+  concat: (x, y) => and(x)(y),
+  empty: always,
+})
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const or: <A>(that: Eq<A>) => (self: Eq<A>) => Eq<A> = that => self =>
+  fromEquals((x, y) => self.equals(x, y) || that.equals(x, y))
+
+/**
+ * @since 2.0.0
+ * @category Instance Methods
+ */
+export const never: Eq<unknown> = fromEquals(() => false)
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const getMonoidAny = <A>(): Monoid<Eq<A>> => ({
+  concat: (x, y) => or(x)(y),
+  empty: never,
+})
+
+/**
+ * @since 2.0.0
+ * @category Instances
+ */
+export const eqStrict: Eq<unknown> = {
+  equals: (x, y) => x === y,
+}
