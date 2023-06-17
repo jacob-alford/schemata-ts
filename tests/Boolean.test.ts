4a7ad1783b2a15aa955c870b66169a6d09caa631
Jacob Alford <jacob.alford@me.com>
2023-06-17 17:45:24 -0600

    add boolean test

---
+++ tests/Boolean.test.ts
@@ -0,0 +1,23 @@
+import * as S from 'schemata-ts'
+import * as JS from 'schemata-ts/JsonSchema'
+
+import { runStandardTestSuite } from '../test-utils/test-suite'
+
+runStandardTestSuite('boolean', S.Boolean, _ => ({
+  decoderTests: [
+    _.decoder.pass(true),
+    _.decoder.pass(false),
+    _.decoder.fail(1),
+    _.decoder.fail('true'),
+    _.decoder.fail('false'),
+    _.decoder.fail({}),
+    _.decoder.fail([]),
+    _.decoder.fail(null),
+    _.decoder.fail(undefined),
+    _.decoder.fail(NaN),
+  ],
+  encoderTests: [],
+  guardTests: [],
+  eqTests: [],
+  jsonSchema: JS.booleanSchema,
+}))()
