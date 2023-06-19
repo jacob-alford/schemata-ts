113f810e4405ec538c29075d88c371e395b09a65
Jacob Alford <jacob.alford@me.com>
2023-06-19 13:11:09 -0600

    adjust lazy, add arbitrary disclaimer

--- src/schemata/Lazy.ts
+++ src/schemata/Lazy.ts
@@ -2,7 +2,9 @@ import { Lazy as LazyArg } from 'fp-ts/function'
 import { make, Schema } from 'schemata-ts/Schema'
 
 /**
- * A lazy schema is a schema for mutual recursive types
+ * A lazy schema is a schema for mutual recursive types.
+ *
+ * **Note: deriving mutually recursive arbitraries is not currently supported**
  *
  * @since 1.0.0
  */
