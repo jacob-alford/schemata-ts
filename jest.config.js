1dbe496c9a8d33c7009bd6dbf6e39661464635c7
Jacob Alford <jacob.alford@me.com>
2023-06-28 20:30:58 -0600

    relax coverage requirements

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.ts'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   moduleNameMapper: {
     '^schemata-ts$': '<rootDir>/src/index.ts',
     '^schemata-ts/(.*)$': '<rootDir>/src/$1',
